1a4c19012232b19715d0e3267c951f090f5ac07f
iamprogrammerlk <iamprogrammerlk@gmail.com>
2024-06-06 07:49:05 +0530

    Build release

---
+++ build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/tmp/sqlite3-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-src"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-build"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/tmp"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/src/sqlite3-populate-stamp"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/src"
+  "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/src/sqlite3-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/src/sqlite3-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/mnt/warehouse/playground/flutter/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/main/flutter_sql_database_storage_using_sqlite_and_sqflite_crud_by_heyflutter_com/build/linux/x64/debug/_deps/sqlite3-subbuild/sqlite3-populate-prefix/src/sqlite3-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
